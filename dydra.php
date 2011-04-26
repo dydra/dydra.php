94b045cc63a489af9c126630bb9591e3d52fe327
Arto Bendiken <arto@bendiken.net>
2011-04-26 23:08:39 +0200

    Created the repository.

---
+++ dydra.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Dydra.com Software Development Kit (SDK) for PHP.
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ */
