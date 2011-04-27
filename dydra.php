d9e41ee54000035897bdf6d82a2539946dbf5657
Arto Bendiken <arto@bendiken.net>
2011-04-27 05:53:12 +0200

    Added object model class stubs.

--- dydra.php
+++ dydra.php
@@ -6,3 +6,43 @@
  *
  * @see http://docs.dydra.com/sdk/php
  */
+
+/**
+ * Implements a Dydra.com XML-RPC API client.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ * @see http://docs.dydra.com/api/rpc
+ */
+class Client {}
+
+/**
+ * Represents a Dydra.com resource.
+ *
+ * This is the base class for all classes that represent dereferenceable
+ * HTTP resources on Dydra.com.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ * @see http://docs.dydra.com/api/rest
+ */
+class Resource {}
+
+/**
+ * Represents a Dydra.com user account.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ */
+class Account extends Resource {}
+
+/**
+ * Represents a Dydra.com RDF repository.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ */
+class Repository extends Resource {}
+
+/**
+ * Represents a Dydra.com job.
+ *
+ * @see http://docs.dydra.com/sdk/php
+ */
+class Job extends Resource {}
